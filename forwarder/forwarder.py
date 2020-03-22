628b9ee072a049ce6cf690a821773f61a8343a74
lexkahng <lexkahng@gmail.com>
2020-03-22 08:13:04 -0700

    adding limbo

--- forwarder/forwarder.py
+++ forwarder/forwarder.py
@@ -3,17 +3,18 @@
 import json
 import socket
 import select
+import logging
 
 def main():
     print("hello")
 
     # Read static variables from json
     myConfig = configObject('config.json')
-    global serverHost, serverPort, forwarderHost, forwarderPort
+    global serverHost, serverPort, finalHost, finalPort
     serverHost = myConfig.serverHost
     serverPort = myConfig.serverPort
-    forwarderHost = myConfig.forwarderHost
-    forwarderPort = myConfig.forwarderPort
+    finalHost = myConfig.finalHost
+    finalPort = myConfig.finalPort
 
     # Create forwarder
     forwarder()
@@ -24,14 +25,14 @@ class configObject:
             data = json.load((config_file))
             self.serverHost = data['server']['host']
             self.serverPort = data['server']['port']
-            self.forwarderHost = data['forwarder']['host']
-            self.forwarderPort = data['forwarder']['port']
+            self.finalHost = data['final']['host']
+            self.finalPort = data['final']['port']
 
 def forwarder():
     print("Server host: ", serverHost)
     print("Server port ", serverPort)
-    print("Forwarder host: ", forwarderHost)
-    print("Forwarder port: ", forwarderPort)
+    print("final host: ", finalHost)
+    print("final port: ", finalPort)
 
     
     # Create server socket
@@ -50,44 +51,54 @@ def forwarder():
     serverSock.setblocking(0) # non-blocking
     serverSock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 
-    # Instantiate 4 dicts
+    # Instantiate 4 dicts for regular tracking
     connections, requests, responses, serverSock_fd = {}, {}, {}, serverSock.fileno()
-    
+
+    # Instantiate final dict for final host fd tracking
+    limbo = {}
+    #trackClient, trackFinal = {}, {}
+
+
     # Continue listening
     while True:
         events = e.poll(1)
         for fd, event in events:
             if fd == serverSock_fd:
                 # initialize connection with client
-
-                ## send connection request to FINAL host (Store state somewhere. possibly another dict)
                 clientConn, clientAddr = serverSock.accept()
                 clientConn.setblocking(0)
                 client_fd = clientConn.fileno()
+
+                # Register client conn to track
                 e.register(client_fd, select.EPOLLIN) # Switch to reading
                 connections[client_fd] = clientConn
+                logging.info("Craeated connection for client")
 
-                # Empty old connection socket, also from FINAL host tracker
-                requests[client_fd] = ''
-                responses[client_fd] = ''
+
+                ## send connection request to FINAL host (Store state somewhere. possibly another dict)
+                finalConn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                finalConn.connect(())
+                finalConn.setblocking(0)
+                final_fd = finalConn.fileno()
+
+                ## Register final host conn to track
+                e.register(finalConn.fileno(), select.EPOLLIN)
+                connections[final_fd] = finalConn
+                logging.info("Craeated connection to final dest")
+
+                ## Added to limbo dict
+                ## how to resolve conflict? 
+                limbo[client_fd] = finalConn # client tracks final
+                limbo[final_fd] = clientConn # final tracks client
 
             elif event & select.EPOLLIN:
-
-                requests[fd] += connections[fd].recv(8) # What should recv be for ssh?
                 
-                # some condition for closing connection (what would it be for ssh?)
-                if requests[fd] == '':
-                    # delete connection
-                    e.unregister(fd)
-                    connections[fd].close()
-                    del connections[fd], requests[fd], responses[fd]
-                    ##  send connection close for FINAL host, remove from tracker
+                # save buffer
+                received = connections[fd].recv(1024) # 2048?
+                logging.info("received data")
 
-
-                #Specify condition for reading (may not need one)
-                else: 
-                    e.modify(fd, select.EPOLLOUT) # Flip to writing
-                    print("more requests coming in. Forwarding")
+                # Forward buffer
+                connections[fd].send(received)
 
             # send response back to original client
             elif event & select.EPOLLOUT:
@@ -95,7 +106,7 @@ def forwarder():
                 responses[fd] = responses[fd][written:]
                 e.modify(fd, select.EPOLLIN) # Flip to reading
 
-            elif event & select.EPOLLHUP:
+            elif event & select.EPOLLHUP | select.EPOLLERR:
                 # close
 
 if __name__ == "__main__":
