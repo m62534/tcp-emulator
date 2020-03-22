5d8d60ce632d1bfbb134b196f9242ca8191406fc
lexkahng <lexkahng@gmail.com>
2020-03-22 12:38:47 -0700

    removed bug

--- forwarder/forwarder.py
+++ forwarder/forwarder.py
@@ -60,61 +60,67 @@ def forwarder():
 
 
     # Continue listening
-    while True:
-        events = epol.poll(1)
-        for fd, event in events:
-            if fd == serverSock_fd:
-                # initialize connection with client
-                clientConn, _ = serverSock.accept()
-                clientConn.setblocking(0)
-                client_fd = clientConn.fileno()
+    try:
+        while True:
+            events = epol.poll(1)
+            for fd, event in events:
+                if fd == serverSock_fd:
+                    # initialize connection with client
+                    clientConn, _ = serverSock.accept()
+                    clientConn.setblocking(0)
+                    client_fd = clientConn.fileno()
 
-                # Register client conn to track
-                epol.register(client_fd, select.EPOLLIN) # Switch to reading
-                connections[client_fd] = clientConn
-                print("Craeated connection for client")
+                    # Register client conn to track
+                    epol.register(client_fd, select.EPOLLIN) # Switch to reading
+                    connections[client_fd] = clientConn
+                    print("Craeated connection for client")
 
 
-                ## send connection request to FINAL host (Store state somewhere. possibly another dict)
-                finalConn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                finalConn.connect((finalHost, finalPort))
-                finalConn.setblocking(0)
-                final_fd = finalConn.fileno()
+                    ## send connection request to FINAL host (Store state somewhere. possibly another dict)
+                    finalConn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    finalConn.connect((finalHost, finalPort))
+                    finalConn.setblocking(0)
+                    final_fd = finalConn.fileno()
 
-                ## Register final host conn to track
-                epol.register(finalConn.fileno(), select.EPOLLIN)
-                connections[final_fd] = finalConn
-                print("Craeated connection to final dest")
+                    ## Register final host conn to track
+                    epol.register(finalConn.fileno(), select.EPOLLIN)
+                    connections[final_fd] = finalConn
+                    print("Craeated connection to final dest")
 
-                ## Added to limbo dict
-                ## Should not conflict
-                limbo[client_fd] = finalConn
-                limbo[final_fd] = clientConn
+                    ## Added to limbo dict
+                    ## Should not conflict
+                    limbo[client_fd] = finalConn
+                    limbo[final_fd] = clientConn
 
-            elif event & select.EPOLLIN:
-                # save buffer
-                received = connections[fd].recv(1024)
+                elif event & select.EPOLLIN:
+                    # save buffer
+                    received = connections[fd].recv(1024)
 
-                # Forward buffer
-                connections[fd].send(received)
+                    # Forward buffer
+                    connections[fd].send(received)
 
-            elif event & select.EPOLLHUP | select.EPOLLERR:
-                # deregister
-                print("deregistering...")
-                epol.unregister(limbo[fd])
-                epol.unregister(fd)
+                elif event & select.EPOLLOUT:
+                    continue
 
-                # close
-                print("closing...")
-                connections[limbo[fd]].close()
-                connections[fd].close()
-                
-                # Release from dicts
-                try:
+                elif event & select.EPOLLHUP:
+                    # deregister
+                    print("deregistering...")
+                    epol.unregister(limbo[fd])
+                    epol.unregister(fd)
+
+                    # close
+                    print("closing...")
+                    connections[limbo[fd]].close()
+                    connections[fd].close()
+                    
+                    # Release from dicts
                     del connections[fd], connections[limbo[fd]], limbo[fd], limbo[limbo[fd]]
-                except Exception as e:
-                    logging.exception('')
-                
+        
+    finally:
+        # Close main socket and epoll
+        epol.unregister(serverSock.fileno())
+        epol.close()
+        serverSock.close()
 
 if __name__ == "__main__":
     main()
